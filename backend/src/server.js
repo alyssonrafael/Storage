726ba3d1b28a8f176534bb0a1c505fa672cce26c
Alysson Rafael <alyssonrafael11@gmail.com>
2024-07-04 15:13:14 -0300

    first commit - preparation of backend - connection of database - table of User

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+require('dotenv').config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use('/api', rotas a serem usadas );
+
+// Teste de conexão com o banco de dados
+async function testDatabaseConnection() {
+  try {
+    await prisma.$connect();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  } finally {
+    await prisma.$disconnect();
+    console.log('Conexão com o banco de dados encerrada.');
+  }
+}
+
+// Inicialização do servidor e o teste de conecxao
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  testDatabaseConnection();
+});
